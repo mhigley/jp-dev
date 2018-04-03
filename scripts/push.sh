71cee5809eabbd177bcb4418a11bb9acb3da6eae
mhigley <mhigley@bitwiseindustries.com>
2018-04-03 13:58:18 -0700

    master

--- scripts/push.sh
+++ scripts/push.sh
@@ -45,7 +45,7 @@ do
         puts-msg 'From branch:' $branch
         puts-cmd git add -A
         # puts-cmd git commit -m "$msg$branch"
-        echo "git commit -m \"$msg$branch\""
+        git commit -m "$msg$branch"
         puts-cmd git push origin $branch
     else
         echo 'else'
